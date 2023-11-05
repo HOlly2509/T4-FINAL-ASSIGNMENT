6bca4c0161090bcc6764b312fce0b89a8808c865
GabrielMelaia231237 <231237@virtualwindow.co.za>
2023-11-05 18:08:36 +0200

    Removed other files, kept my own
    Committing my own files, my javascript and html pages

---
+++ js/watchlist.js
@@ -0,0 +1,65 @@
+const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+
+fetch(API_URL)
+    .then(response => response.json())
+    .then(data => displayMovies(data.results));
+
+function displayMovies(movies) {
+    const availableMovies = document.getElementById('available-movies');
+    availableMovies.innerHTML = '';
+
+    movies.forEach(movie => {
+        const movieCard = document.createElement('div');
+        movieCard.classList.add('movie-card');
+
+        movieCard.innerHTML = `
+            <img src="${IMG_URL + movie.poster_path}" alt="${movie.title}">
+            <div class="movie-description">
+                <h1>${movie.title}</h1>
+                <p>${movie.overview}</p>
+                <button onclick="addToWatchlist('${movie.title}', '${movie.poster_path}', '${movie.overview}')">Add to Watchlist</button>
+            </div>
+        `;
+
+        availableMovies.appendChild(movieCard);
+    });
+}
+
+function addToWatchlist(title, posterPath, overview) {
+    const watchlist = document.getElementById('watchlist');
+
+    const movieCard = document.createElement('div');
+    movieCard.classList.add('movie-card');
+
+    movieCard.innerHTML = `
+        <img src="${IMG_URL + posterPath}" alt="${title}">
+        <div class="movie-description">
+            <h1>${title}</h1>
+            <p>${overview}</p>
+            <button onclick="removeFromWatchlist(this.parentElement.parentElement)">Remove from Watchlist</button>
+        </div>
+    `;
+
+    watchlist.appendChild(movieCard);
+    showPopup(`${title} has been added to your watchlist!`);
+}
+
+function removeFromWatchlist(movieCardElement) {
+    movieCardElement.remove();
+}
+
+function showPopup(message) {
+    const popup = document.createElement('div');
+    popup.classList.add('popup');
+    popup.textContent = message;
+
+    document.body.appendChild(popup);
+
+    setTimeout(() => {
+        popup.remove();
+    }, 3000); 
+}
